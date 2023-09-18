0f2173cc8cb2d426a643cd986677147b2863987b
sayantan-b <sayantan.bose@bitpack.me>
2023-09-18 15:16:06 +0530

    Move Project from fork

---
+++ README.md
@@ -0,0 +1,103 @@
+<p align="center">
+  <picture>
+    <img alt="Textbase python library" src="assets/logo.svg" width="352" height="59" style="max-width: 100%;">
+  </picture>
+  <br/>
+  <br/>
+</p>
+
+<h3 align="center">
+    <p>✨ Textbase is a framework for building chatbots using NLP and ML. ✨</p>
+</h3>
+
+# CHATBOT MOVIE RECOMMENDER
+
+This chatbot is designed to recommend movies and is built on top of the Textbase UI using the provided starter code. It can be seamlessly integrated with OTT platforms like Netflix or Disney Hotstar to offer movie recommendations without users having to search extensively for movies. The implemented features include:
+
+Recommending movies based on genres selected by users.
+Recommending movies based on actor or actress names provided by users.
+The chatbot's user-friendly interface aims to provide a personalized movie-watching experience by catering to the unique preferences of each user. It simplifies the process of discovering and enjoying movies, ultimately enhancing the overall movie-watching experience.
+
+Additionally, the chatbot has the potential for future expansion and improvement, including the integration of advanced recommendation algorithms and customizable options for users. This chatbot seeks to streamline the movie discovery process and make it more convenient for users to find and enjoy films.
+
+## Implementation
+
+The chatbot is implemented in the following way:
+User Input: The chatbot takes user input as its initial step.
+
+- Query Matching: It then checks if the user input matches any predefined queries. These queries likely serve as triggers for specific actions or responses.
+
+- Movie Relatedness: The chatbot determines if the user's input is related to movies. If it's not movie-related, it may provide a generic response or offer assistance with non-movie queries.
+
+- GPT Query: If the input is movie-related, the chatbot sends a query to the GPT model (likely for generating recommendations or responses).
+
+- Movie Recommendations: The GPT model returns a list of movie names based on the user's input, which are potentially related to the user's movie preference or query.
+
+- OMDB API Integration: The chatbot then connects to the OMDB API using these movie names to search for detailed information about the movies, including titles, IMDb URLs, release years, and types.
+
+- Output to User: Finally, the chatbot compiles the movie details obtained from the OMDB API and presents them as recommendations or information to the user.
+
+## Models Used
+
+1. text-davinci-003
+2. gpt-3.5-turbo
+
+## Installation
+
+Make sure you have `python version >=3.9.0`, it's always good to follow the [docs](https://docs.textbase.ai/get-started/installation) 👈🏻
+
+### 1. Through pip
+
+```bash
+pip install textbase-client
+```
+
+### 2. Local installation
+
+Clone the repository and install the dependencies using [Poetry](https://python-poetry.org/) (you might have to [install Poetry](https://python-poetry.org/docs/#installation) first).
+
+For proper details see [here]()
+
+```bash
+git clone https://github.com/cofactoryai/textbase
+cd textbase
+poetry shell
+poetry install
+```
+
+## Start development server
+
+> If you're using the default template, **remember to set the OpenAI API key** in `main.py`.
+
+Run the following command:
+
+- if installed locally
+  ```bash
+  poetry run python textbase/textbase_cli.py test
+  ```
+- if installed through pip
+  `bash
+textbase-client test
+`
+  Response:
+
+```bash
+Path to the main.py file: textbase/main.py # You can create a main.py by yourself and add that path here. NOTE: The path should not be in quotes
+```
+
+## Changes Made
+
+1. The models.py has been updated.
+2. The constants.py has been added.
+3. The main.py has been added.
+
+## Video Demonstration
+
+https://drive.google.com/file/d/1NQXP7oDfXDgIqu6AD8olgUBsWd_YIV1n/view?usp=sharing
+
+## Future Work
+
+There are a lot of improvements that can be done :
+
+- Implementing user customization is a great idea. Allowing users to specify their movie preferences, such as favorite genres, actors, directors, and even movie ratings, can lead to highly personalized movie recommendations.
+- Exploring and integrating more advanced movie recommendation models can significantly improve the quality and accuracy of movie suggestions. Models like collaborative filtering, matrix factorization, or deep learning-based recommenders can be considered.
---
+++ textbase/models.py
@@ -0,0 +1,156 @@
+from pickle import TRUE
+import openai
+import requests
+import re
+from textbase import Message
+from constants import *
+
+# Global Methods
+
+
+def get_contents(message: Message, data_type: str) -> list:
+    """
+    Get contents of a specific data_type from a message.
+    """
+
+    return [
+        {
+            "role": message["role"],
+            "content": content["value"]
+        }
+        for content in message["content"]
+        if content["data_type"] == data_type
+    ]
+
+
+def recommendMovies(genre: str) -> str:
+    """
+    Recommend movies based on genre using the OMDB API.
+    """
+
+    omdb_api_key = ""
+    omdb_url = f"http://www.omdbapi.com/?s={genre}&apikey={omdb_api_key}"
+    print(omdb_url)
+    response = requests.get(omdb_url)
+    print(response)
+    data = response.json()
+    recommended_movies = []
+    if "Search" in data:
+        for movie in data["Search"]:
+            print(movie)
+            title = movie.get("Title", "")
+            imdb_id = movie.get("imdbID", "")
+            year = movie.get("Year", "")
+            type = movie.get("Type", "")
+            imdb_url = f"https://www.imdb.com/title/{imdb_id}/"
+            recommended_movies.append({
+                "title": title,
+                "imdb_url": imdb_url,
+                "year": year,
+                "type": type,
+            })
+    return recommended_movies
+
+
+class OpenAI:
+    api_key = None
+
+    @classmethod
+    def checkStatementIsRelatedToMovie(cls, user_message):
+        """
+        gpt model to check if the input text is related to movies
+        """
+
+        if (any(keyword in user_message for keyword in yesNoQuestionList)):
+            return TRUE
+
+        if (any(keyword in user_message for keyword in genres)):
+            return TRUE
+
+        # Define a prompt to frame your question
+        prompt = f"{BASE_PROMPT} '{user_message}'"
+
+        # Use GPT-3.5 Turbo to check if the sentence is related to movies
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # You can use GPT-3.5 Turbo for faster results
+            prompt=prompt,
+            max_tokens=1,  # Set max_tokens to 1 to get a brief answer
+        )
+        print(response)
+        # Check the model's completion for a relevant response
+        is_related_to_movies = "yes" in response.choices[0].text.lower()
+        print(is_related_to_movies)
+        return is_related_to_movies
+
+    @classmethod
+    def generate(
+        cls,
+        system_prompt: str,
+        message_history: list[Message],
+        model="gpt-3.5-turbo",
+        max_tokens=1000,
+        temperature=0.7,
+    ):
+        assert cls.api_key is not None, "OpenAI API key is not set."
+        openai.api_key = cls.api_key
+        user_message = user_message = message_history[-1]["content"][0]["value"].lower(
+        )
+        filtered_messages = []
+
+        if any(keyword in user_message for keyword in suggestionKeywordList):
+            return SUGGEST_KEYWORD_PROMPT
+
+        if any(keyword in user_message for keyword in whoKeywordList):
+            return WHO_KEYWORD_PROMPT
+
+        if any(keyword in user_message for keyword in howKeywordList):
+            return HOW_KEYWORD_PROMPT
+
+        if any(keyword in user_message for keyword in whatKeywordList):
+            return WHAT_KEYWORD_PROMPT
+
+        if any(keyword in user_message for keyword in welcomeKeywordList):
+            return WELCOME_KEYWORD_PROMPT
+
+        if cls.checkStatementIsRelatedToMovie(user_message) == TRUE:
+            for message in message_history:
+                # list of all the contents inside a single message
+                contents = get_contents(message, "STRING")
+                if contents:
+                    filtered_messages.extend(contents)
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": system_prompt
+                    },
+                    *map(dict, filtered_messages),
+                ],
+                temperature=temperature,
+                max_tokens=max_tokens,
+            )
+            result = response["choices"][0]["message"]["content"]
+            pattern = r'"([^"]+)"'
+            movie_names = re.findall(pattern, result)
+            recommended_movies_list = []
+            for name in movie_names:
+                recommended_movies = recommendMovies(name)
+                recommended_movies_list.append(recommended_movies)
+            if recommended_movies_list:
+                response = "Here are some movie recommendations for you:\n\n"
+                for movies in recommended_movies_list:
+                    for index, movie in enumerate(movies):
+                        response += f"● Recommended Movie - {index + 1}\n\n"
+                        response += f" Title: {movie['title']}\n\n"
+                        response += f" IMDb URL: {movie['imdb_url']}\n\n"
+                        response += f" Release Year: {movie['year']}\n\n"
+                        response += f" Type: {movie['type']}\n\n"
+                return response
+            if result:
+                return result
+            else:
+                return MOVIE_NOT_FOUND_PROMPT
+
+        else:
+            return NOT_RELATED_MOVIE_PROMPT
